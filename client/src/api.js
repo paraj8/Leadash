e405e4330df88909e8c667b0ff26f36ed9acfeee
paraj8 <paraj8691@gmail.com>
2026-05-26 15:02:35 +0530

    Added environment variable to shitch bitween local to production server

--- client/src/api.js
+++ client/src/api.js
@@ -1,38 +1,16 @@
-
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "https://leadash.onrender.com/api",        // production
- //baseURL: "http://localhost:5000/api"            //local
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
-{/*
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return req;
-});
-
-
-
-*/}
-
 
 API.interceptors.request.use((req) => {
 
   const token = localStorage.getItem("token");
 
-  console.log("TOKEN:", token);
-
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
-
-  console.log(req.headers);
 
   return req;
 });
