8fae37b75e5a2258bd4746639ef20a7ec6155a9c
paraj8 <paraj8691@gmail.com>
2026-06-01 11:57:45 +0530

    Removed TLS authintication

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -4,7 +4,6 @@ const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
   secure: false,
-  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
