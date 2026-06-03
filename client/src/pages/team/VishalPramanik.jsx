bd116522a08d825283507408153e3ecf4f4eaacf
paraj8 <paraj8691@gmail.com>
2026-06-04 01:13:50 +0530

    Croped developer's face image

--- client/src/pages/team/VishalPramanik.jsx
+++ client/src/pages/team/VishalPramanik.jsx
@@ -1,17 +1,17 @@
 
 /*import vishalImg from ".src/assets/vishal.jpg";*/
-export default function VishalPramik() {
+export default function VishalPramanik() {
   return (
     <div className="min-h-screen bg-slate-950 text-white flex items-center justify-center px-6">
       <div className="max-w-3xl w-full bg-slate-900 p-10 rounded-3xl border border-white/10">
         <div className="flex flex-col items-center">
           <img
             src="../src/assets/vishal.jpg"
-            alt="Vishal Pramik"
+            alt="Vishal Pramanik"
             className="w-48 h-48 rounded-full object-cover border-4 border-cyan-400 mb-6"
           />
           <h1 className="text-4xl font-bold mb-2">
-            Vishal Pramik
+            Vishal Pramanik
           </h1>
           <p className="text-cyan-400 text-lg mb-6">
             Manager
